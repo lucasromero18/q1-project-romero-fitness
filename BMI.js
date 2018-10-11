f4e59248367fb28946591c16363d872c07240684
unknown <lucasromero65@yahoo.com>
2018-10-10 22:01:55 -0500

    HTML and CSS done

--- BMI.js
+++ BMI.js
@@ -1,9 +1,1 @@
 
-axios.get("https://wger.de/api/v2/exercise/", {
-  header: {
-    authorization: "Token b42f25822a561ae2858762328ad8c2bc709c0853"
-  }
-}).then((res) => {
-    console.log(res);
-
-  })
