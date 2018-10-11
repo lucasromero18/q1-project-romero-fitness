52a282361315bc6420c4e9e59ffd39376d2c70d5
unknown <lucasromero65@yahoo.com>
2018-10-11 11:13:46 -0500

    starting on the third page, first and second finished

--- workout.js
+++ workout.js
@@ -1,4 +1,4 @@
-axios.get("https://wger.de/api/v2/exercise/", {
+axios.get("https://wger.de/api/v2/exercise/?language=2", {
   header: {
     authorization: "Token b42f25822a561ae2858762328ad8c2bc709c0853"
   }
